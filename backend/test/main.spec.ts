eb7ed6ca7d21d6e8412e976ea667386398d72ba8
LUCCA-LOLDESIGN <mario.padrao-loldesign@fretebras.com.br>
2024-02-23 20:44:15 -0300

    refactor: add signup-input builder

--- backend/test/main.spec.ts
+++ backend/test/main.spec.ts
@@ -1,14 +1,6 @@
-import { faker } from "@faker-js/faker";
-import { cpf } from "cpf-cnpj-validator";
 import { signup } from "../src/main";
-
-interface SignupInput {
-  name: string;
-  email: string;
-  cpf: string;
-  isPassenger: boolean;
-  password: string;
-}
+import { SignupInput } from "../src/dtos/signup-input";
+import { SignUpInputBuilder } from "./builders/signup-input-builder";
 
 interface Fixture {
   signupInput: SignupInput;
@@ -16,13 +8,7 @@ interface Fixture {
 
 const createFixture = (): Fixture => {
   return {
-    signupInput: {
-      name: faker.person.fullName(),
-      cpf: cpf.generate(false),
-      email: faker.internet.email(),
-      isPassenger: true,
-      password: faker.internet.password(),
-    },
+    signupInput: new SignUpInputBuilder().build(),
   };
 };
 
@@ -36,5 +22,9 @@ describe("Main", () => {
 
     // then
     expect(signupOutput.accountId).toBeDefined();
+    expect(signupOutput.name).toBe(signupInput.name);
+    expect(signupOutput.email).toBe(signupInput.email);
+    expect(signupOutput.cpf).toBe(signupInput.cpf);
+    expect(signupOutput.isPassenger).toBe(signupInput.isPassenger);
   });
 });
