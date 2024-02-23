baf67c27a3110e91d4dacee1e13d83489c06dd29
LUCCA-LOLDESIGN <mario.padrao-loldesign@fretebras.com.br>
2024-02-23 19:52:02 -0300

    test: add Main tests with TDD methodology

--- backend/test/main.spec.ts
+++ backend/test/main.spec.ts
@@ -1,9 +1,40 @@
+import { faker } from "@faker-js/faker";
+import { cpf } from "cpf-cnpj-validator";
+import { signup } from "../src/main";
+
+interface SignupInput {
+  name: string;
+  email: string;
+  cpf: string;
+  isPassenger: boolean;
+  password: string;
+}
+
+interface Fixture {
+  signupInput: SignupInput;
+}
+
+const createFixture = (): Fixture => {
+  return {
+    signupInput: {
+      name: faker.person.fullName(),
+      cpf: cpf.generate(false),
+      email: faker.internet.email(),
+      isPassenger: true,
+      password: faker.internet.password(),
+    },
+  };
+};
+
 describe("Main", () => {
-    it("should create account for passenger", () => {
-        // given
+  it("should create account for passenger", async () => {
+    // given
+    const { signupInput } = createFixture();
 
-        // when
+    // when
+    const signupOutput = await signup(signupInput);
 
-        // then
-    })
-})
+    // then
+    expect(signupOutput.accountId).toBeDefined();
+  });
+});
