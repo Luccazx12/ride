bd8dbf40a728da6837761d0fa2a036e77a63e02d
LUCCA-LOLDESIGN <mario.padrao-loldesign@fretebras.com.br>
2024-02-25 19:09:49 -0300

    refactor(signup): replace random number errors to javascript Error

--- backend/test/main.spec.ts
+++ backend/test/main.spec.ts
@@ -2,6 +2,7 @@ import { getAccountById, signup } from "../src/main";
 import { SignupInput } from "../src/dtos/signup-input";
 import { SignUpInputBuilder } from "./builders/signup-input-builder";
 import { faker } from "@faker-js/faker";
+import { SignupOutput } from "../src/dtos/signup-output";
 
 interface Fixture {
   signupInput: SignupInput;
@@ -22,11 +23,11 @@ describe("Main", () => {
       .build();
 
     // when
-    const signupOutput = await signup(signupInput);
+    const signupOutput = (await signup(signupInput)) as SignupOutput;
 
     // then
+    expect(signupOutput).toHaveProperty("accountId");
     const account = await getAccountById(signupOutput.accountId);
-    expect(signupOutput.accountId).toBeDefined();
     expect(account.name).toBe(signupInput.name);
     expect(account.email).toBe(signupInput.email);
     expect(account.cpf).toBe(signupInput.cpf);
@@ -41,18 +42,18 @@ describe("Main", () => {
       .build();
 
     // when
-    const signupOutput = await signup(signupInput);
+    const signupOutput = (await signup(signupInput)) as SignupOutput;
 
     // then
+    expect(signupOutput).toHaveProperty("accountId");
     const account = await getAccountById(signupOutput.accountId);
-    expect(signupOutput.accountId).toBeDefined();
     expect(account.name).toBe(signupInput.name);
     expect(account.email).toBe(signupInput.email);
     expect(account.cpf).toBe(signupInput.cpf);
     expect(account.is_passenger).toBe(signupInput.isPassenger);
   });
 
-  it("should return -4 when email is already related to an account", async () => {
+  it("should return Error when email is already related to an account", async () => {
     // given
     const { signupInput } = createFixture();
     await signup(signupInput);
@@ -64,12 +65,12 @@ describe("Main", () => {
     const signupOutput = await signup(inputWithSameEmail);
 
     // then
-    const account = await getAccountById(signupOutput.accountId);
-    expect(signupOutput).toBe(-4);
-    expect(account).not.toBeDefined();
+    expect(signupOutput).toEqual(
+      new Error("Account with this email already exists")
+    );
   });
 
-  it("should return -3 when name is invalid", async () => {
+  it("should return Error when name is invalid", async () => {
     // given
     const invalidName = faker.number.int().toString();
     const signupInput = new SignUpInputBuilder().withName(invalidName).build();
@@ -78,12 +79,10 @@ describe("Main", () => {
     const signupOutput = await signup(signupInput);
 
     // then
-    const account = await getAccountById(signupOutput.accountId);
-    expect(account).not.toBeDefined();
-    expect(signupOutput).toBe(-3);
+    expect(signupOutput).toEqual(new Error("Invalid name"));
   });
 
-  it("should return -2 when email is invalid", async () => {
+  it("should return Error when email is invalid", async () => {
     // given
     const invalidEmail = faker.lorem.words();
     const signupInput = new SignUpInputBuilder()
@@ -94,9 +93,7 @@ describe("Main", () => {
     const signupOutput = await signup(signupInput);
 
     // then
-    const account = await getAccountById(signupOutput.accountId);
-    expect(signupOutput).toBe(-2);
-    expect(account).not.toBeDefined();
+    expect(signupOutput).toEqual(new Error("Invalid email"));
   });
 
   it.each([
@@ -107,7 +104,7 @@ describe("Main", () => {
     "1111",
     "111111111",
     "11111111111",
-  ])("should return -1 when cpf is invalid", async (invalidCpf) => {
+  ])("should return Error when cpf is invalid", async (invalidCpf) => {
     // given
     const signupInput = new SignUpInputBuilder().withCpf(invalidCpf).build();
 
@@ -115,12 +112,10 @@ describe("Main", () => {
     const signupOutput = await signup(signupInput);
 
     // then
-    const account = await getAccountById(signupOutput.accountId);
-    expect(signupOutput).toBe(-1);
-    expect(account).not.toBeDefined();
+    expect(signupOutput).toEqual(new Error("Invalid CPF"));
   });
 
-  it("should return -5 when carPlate is invalid and account is driver", async () => {
+  it("should return Error when carPlate is invalid and account is driver", async () => {
     // given
     const invalidCarPlate = faker.lorem.words();
     const signupInput = new SignUpInputBuilder()
@@ -132,8 +127,6 @@ describe("Main", () => {
     const signupOutput = await signup(signupInput);
 
     // then
-    const account = await getAccountById(signupOutput.accountId);
-    expect(signupOutput).toBe(-5);
-    expect(account).not.toBeDefined();
+    expect(signupOutput).toEqual(new Error("Invalid car plate"));
   });
 });
