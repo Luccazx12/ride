a74fe4f2ca644fa37729e67a9abce5c4b2dcf8e2
LUCCA-LOLDESIGN <mario.padrao-loldesign@fretebras.com.br>
2024-02-25 22:26:17 -0300

    refactor: mapping data to uncople application layer from database columns

--- backend/test/doubles/in-memory-account-dao.ts
+++ backend/test/doubles/in-memory-account-dao.ts
@@ -1,15 +1,15 @@
-import { AccountDAO } from "../../src/DAO/account-dao";
+import { Account, AccountDAO } from "../../src/DAO/account-dao";
 
 export class InMemoryAccountDAO implements AccountDAO {
   private accounts: any[] = [];
 
-  public async getByEmail(email: string): Promise<any> {
+  public async getByEmail(email: string): Promise<Account | null> {
     return this.accounts.find((account: any) => account.email === email);
   }
-  public async getById(id: string): Promise<any> {
+  public async getById(id: string): Promise<Account | null> {
     return this.accounts.find((account: any) => account.accountId === id);
   }
-  public async save(account: any): Promise<void> {
+  public async save(account: Account): Promise<void> {
     this.accounts.push(account);
   }
 }
---
+++ backend/test/e2e/signup.e2e.spec.ts
@@ -0,0 +1,7 @@
+describe("Signup (e2e)", () => {
+  it("should create an passenger account", () => {
+    // given
+    // when
+    // then
+  });
+});
