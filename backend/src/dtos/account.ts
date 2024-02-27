eef58a3eca9dfe6c1f335abdbf0b889f192250d8
LUCCA-LOLDESIGN <mario.padrao-loldesign@fretebras.com.br>
2024-02-27 13:12:08 -0300

    refactor: change typing from interface to type

--- backend/src/dtos/account.ts
+++ backend/src/dtos/account.ts
@@ -1,4 +1,4 @@
-export interface Account {
+export type Account = {
   accountId: string;
   name: string;
   email: string;
--- backend/src/dtos/request-ride-output.ts
+++ backend/src/dtos/request-ride-output.ts
@@ -1,3 +1,3 @@
-export interface RequestRideOutput {
+export type RequestRideOutput = {
   rideId: string;
 }
