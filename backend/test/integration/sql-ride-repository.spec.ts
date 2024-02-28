7a8d40d11cfef4f8dabfee44f35106e28378d799
LUCCA-LOLDESIGN <mario.padrao-loldesign@fretebras.com.br>
2024-02-28 02:08:47 -0300

    test(ride): add update and listByDriverId methods tests in ride-repository

--- backend/test/integration/sql-ride-repository.spec.ts
+++ backend/test/integration/sql-ride-repository.spec.ts
@@ -1,11 +1,11 @@
 import { faker } from "@faker-js/faker";
 import { SqlRideRepository } from "../../src/infrastructure/repository/ride-repository";
 import { RideBuilder } from "../builders/ride-builder";
-import { RideStatus } from "../../src/dtos/ride";
 import {
   DatabaseConnection,
   PgPromiseAdapter,
 } from "../../src/infrastructure/database/database-connection";
+import { RideStatus } from "../../src/domain/entity/ride";
 
 describe("SqlRideRepository (integration)", () => {
   let databaseConnection: DatabaseConnection;
@@ -19,27 +19,45 @@ describe("SqlRideRepository (integration)", () => {
   describe("save", () => {
     it("should create ride", async () => {
       // given
-      const dao = new SqlRideRepository(databaseConnection);
+      const repository = new SqlRideRepository(databaseConnection);
       const ride = new RideBuilder().build();
 
       // when
-      await dao.save(ride);
+      await repository.save(ride);
 
       // then
-      const foundRide = await dao.getById(ride.rideId);
+      const foundRide = await repository.getById(ride.rideId);
       expect(foundRide).toEqual(ride);
+    });
+  });
+
+  describe("update", () => {
+    it("should update ride", async () => {
+      // given
+      const repository = new SqlRideRepository(databaseConnection);
+      const ride = new RideBuilder().build();
+      await repository.save(ride);
+      const updatedRide = new RideBuilder().withId(ride.rideId).build();
+
+      // when
+      await repository.update(updatedRide);
+
+      // then
+      const foundRide = await repository.getById(ride.rideId);
+      expect(foundRide).not.toEqual(ride);
+      expect(foundRide).toEqual(updatedRide);
     });
   });
 
   describe("getById", () => {
     it("should get ride by id", async () => {
       // given
-      const dao = new SqlRideRepository(databaseConnection);
+      const repository = new SqlRideRepository(databaseConnection);
       const ride = new RideBuilder().build();
-      await dao.save(ride);
+      await repository.save(ride);
 
       // when
-      const foundRide = await dao.getById(ride.rideId);
+      const foundRide = await repository.getById(ride.rideId);
 
       // then
       expect(foundRide).toBeDefined();
@@ -48,10 +66,10 @@ describe("SqlRideRepository (integration)", () => {
 
     it("should return null when ride is not found by id", async () => {
       // given
-      const dao = new SqlRideRepository(databaseConnection);
+      const repository = new SqlRideRepository(databaseConnection);
 
       // when
-      const foundRide = await dao.getById(faker.string.uuid());
+      const foundRide = await repository.getById(faker.string.uuid());
 
       // then
       expect(foundRide).toBeNull();
@@ -61,17 +79,16 @@ describe("SqlRideRepository (integration)", () => {
   describe("listByPassengerId", () => {
     it("should list rides by passengerId and status", async () => {
       // given
-      const dao = new SqlRideRepository(databaseConnection);
+      const repository = new SqlRideRepository(databaseConnection);
       const firstRide = new RideBuilder().build();
       const secondRide = new RideBuilder()
         .withPassengerId(firstRide.passengerId)
-        .withStatus(firstRide.status)
         .build();
-      await dao.save(firstRide);
-      await dao.save(secondRide);
+      await repository.save(firstRide);
+      await repository.save(secondRide);
 
       // when
-      const rides = await dao.listByPassengerId(
+      const rides = await repository.listByPassengerId(
         firstRide.passengerId,
         firstRide.status
       );
@@ -84,13 +101,50 @@ describe("SqlRideRepository (integration)", () => {
 
     it("should return empty array when rides is not found", async () => {
       // given
-      const dao = new SqlRideRepository(databaseConnection);
+      const repository = new SqlRideRepository(databaseConnection);
 
       // when
-      const rides = await dao.listByPassengerId(
+      const rides = await repository.listByPassengerId(
         faker.string.uuid(),
         RideStatus.accepted
       );
+
+      // then
+      expect(rides.length === 0).toBeTruthy();
+    });
+  });
+
+  describe("listByDriverId", () => {
+    it("should list rides by driverId and status", async () => {
+      // given
+      const driverId = faker.string.uuid();
+      const repository = new SqlRideRepository(databaseConnection);
+      const firstRide = new RideBuilder().withDriverId(driverId).build();
+      const secondRide = new RideBuilder()
+        .withDriverId(firstRide.driverId)
+        .build();
+      await repository.save(firstRide);
+      await repository.save(secondRide);
+
+      // when
+      const rides = await repository.listByDriverId(driverId, [
+        firstRide.status,
+      ]);
+
+      // then
+      const expectedPersistedRides = [firstRide, secondRide];
+      expect(rides.length === expectedPersistedRides.length).toBeTruthy();
+      expect(rides).toEqual(expectedPersistedRides);
+    });
+
+    it("should return empty array when rides is not found", async () => {
+      // given
+      const repository = new SqlRideRepository(databaseConnection);
+
+      // when
+      const rides = await repository.listByDriverId(faker.string.uuid(), [
+        RideStatus.accepted,
+      ]);
 
       // then
       expect(rides.length === 0).toBeTruthy();
