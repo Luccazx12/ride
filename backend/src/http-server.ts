a43d3c1990f227695f955d4b03c870c1ed94ba5c
LUCCA-LOLDESIGN <mario.padrao-loldesign@fretebras.com.br>
2024-02-27 22:05:24 -0300

    refactor: uncouple express from interface adapters layer

---
+++ backend/src/http-server.ts
@@ -0,0 +1,63 @@
+import express from "express";
+
+export enum HttpMethod {
+  get = "get",
+  post = "post",
+  put = "put",
+  patch = "patch",
+}
+
+export type HttpResponse = {
+  status: number;
+  data?: unknown;
+};
+
+export type HttpRequest = {
+  body?: any;
+  query?: any;
+  params?: any;
+  headers?: any;
+};
+
+type HttpFunction = (request: HttpRequest) => Promise<HttpResponse>;
+
+export interface HttpServer {
+  listen(port: number): void;
+  register(method: HttpMethod, url: string, callback: HttpFunction): void;
+}
+
+export class ExpressAdapter implements HttpServer {
+  private readonly app: express.Express;
+
+  public constructor() {
+    this.app = express();
+    this.app.use(express.json());
+  }
+
+  public listen(port: number): void {
+    this.app.listen(port);
+  }
+
+  public register(
+    method: HttpMethod,
+    url: string,
+    callback: HttpFunction
+  ): void {
+    this.app[method](url, async function (req, res) {
+      try {
+        const callbackOutput = await callback({
+          body: req.body,
+          params: req.params,
+          query: req.query,
+          headers: req.headers,
+        });
+        res.status(callbackOutput.status).json(callbackOutput.data);
+      } catch (error: unknown) {
+        res.status(500).json({
+          code: "internal_server_error",
+          message: "Internal server error",
+        });
+      }
+    });
+  }
+}
