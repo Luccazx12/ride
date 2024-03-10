79d8b6ae2e12dea87815b3a006140aa22c03d462
LUCCA-LOLDESIGN <mario.padrao-loldesign@fretebras.com.br>
2024-03-09 21:29:03 -0300

    feat(dependencies): add dependency injection container

---
+++ backend/src/infrastructure/dependency-injection/di-tokens.ts
@@ -0,0 +1,5 @@
+export const HttpServerDIToken = Symbol("HTTP_SERVER");
+export const SignupDIToken = Symbol("SIGN_UP");
+export const GetAccountDIToken = Symbol("GET_ACCOUNT");
+export const GetRideDIToken = Symbol("GET_ACCOUNT");
+export const RequestRideDIToken = Symbol("REQUEST_RIDE");
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,7 +8,17 @@ import { SqlAccountRepository } from "./infrastructure/repository/account-reposi
 import { SqlRideRepository } from "./infrastructure/repository/ride-repository";
 import { RequestRide } from "./application/usecase/request-ride";
 import { Signup } from "./application/usecase/signup";
+import { Container } from "./infrastructure/dependency-injection/container";
+import { InMemoryContainerStorage } from "./infrastructure/dependency-injection/in-memory-container-storage";
+import {
+  GetAccountDIToken,
+  HttpServerDIToken,
+  SignupDIToken,
+  RequestRideDIToken,
+  GetRideDIToken,
+} from "./infrastructure/dependency-injection/di-tokens";
 
+const container = Container.getInstance(new InMemoryContainerStorage());
 const connection = new PgPromiseAdapter();
 const signup = new Signup(
   new SqlAccountRepository(connection),
@@ -19,20 +29,16 @@ const getRide = new GetRide(
   new SqlRideRepository(connection),
   new SqlAccountRepository(connection)
 );
-
 const requestRide = new RequestRide(
   new SqlAccountRepository(connection),
   new SqlRideRepository(connection)
 );
-
 const httpServer = new ExpressAdapter();
-
-const mainController = new MainController(
-  httpServer,
-  signup,
-  getAccount,
-  requestRide,
-  getRide
-);
+container.register(HttpServerDIToken, httpServer);
+container.register(SignupDIToken, signup);
+container.register(GetAccountDIToken, getAccount);
+container.register(GetRideDIToken, getRide);
+container.register(RequestRideDIToken, requestRide);
+const mainController = new MainController();
 mainController.registerHttpRoutes();
 httpServer.listen(3000);
